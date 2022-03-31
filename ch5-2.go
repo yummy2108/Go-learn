c1a97aa9abd747fee4a3c074b22c5ae8981cd59b
Mingjing Yu <mingjingyu@microsoft.com>
2022-03-31 14:43:21 +0800

    go

---
+++ ch5-2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	people := []Person{
+		{"A", "C", 1},
+		{"B", "B", 2},
+		{"C", "A", 3},
+	}
+	fmt.Println(people)
+
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].LastName < people[j].LastName
+	})
+
+	fmt.Println(people)
+}
